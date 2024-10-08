90a7d12b6629dd45ccbb31ac3f84aefca025c3cc
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-10-08 21:00:40 +0300

    функция introspection_info возвращает словарь содержащий информацию об объекте

---
+++ main.py
@@ -0,0 +1,13 @@
+import inspect
+from pprint import pprint
+def introspection_info(obj):
+    all_attrs = dir(obj)
+    methods = [attr for attr in all_attrs if callable(getattr(obj, attr)) and not attr.startswith("__")]
+    attributes = [attr for attr in all_attrs if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    modul = inspect.getmodule(introspection_info)
+    return {'type':type(obj),'attributes':attributes,'methods':methods,'modul': modul}
+
+
+
+number_info = introspection_info(53)
+print(number_info)
